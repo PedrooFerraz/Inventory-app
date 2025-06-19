76d7d505826c326b3dc0c6b9bcfcc119078576f6
Pedro Ferraz <pepito.ferraz@gmail.com>
2025-06-19 16:57:41 -0300

    .

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context"
 export default function Layout() {
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#182234" }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#4f6a92" }}>
             <StatusBar
                 animated={true}
                 backgroundColor="#FFF"
