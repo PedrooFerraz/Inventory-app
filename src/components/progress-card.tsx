90dae9c121570bad3da85907f1162462907543a2
Pedro Ferraz <pepito.ferraz@gmail.com>
2025-07-25 16:58:48 -0300

    Fix: Dependendo do tamanho da tela do celular os numeros de progresso na hora da contagem estavam passando da borda

--- src/components/progress-card.tsx
+++ src/components/progress-card.tsx
@@ -3,7 +3,7 @@ import { ColorValue, StyleSheet, Text, View } from "react-native";
 export default function ProgressCard({label, number, color} : {label:string, number: number, color: ColorValue | undefined}) {
     return (
         <View style={styles.progressStatus}>
-            <Text style={{ fontSize: 20, color: color, fontWeight: "700" }}>{number}</Text>
+            <Text style={{ fontSize: 17, color: color, fontWeight: "700" }}>{number}</Text>
             <Text style={{ fontSize: 12, color: "rgba(255, 255, 255, 0.85)" }}>{label}</Text>
         </View>
     )
