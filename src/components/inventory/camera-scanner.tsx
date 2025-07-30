251fdb2b3e457b9e883258971992aaea8704cdd0
Pedro Ferraz <pepito.ferraz@gmail.com>
2025-07-30 18:40:17 -0300

    .

--- src/components/inventory/camera-scanner.tsx
+++ src/components/inventory/camera-scanner.tsx
@@ -86,14 +86,14 @@ export default function CameraScanner({
   const handleBarcodeScanned = (e: any) => {
     if (cooldownRef.current || !isScanning) return;
 
-    const {origin, size} = e.bounds
+    const { origin, size } = e.bounds
 
     setX(origin.x)
     setY(origin.y)
     setWidth(size.width)
     setHeight(size.height)
 
-    const isIn = isInScannableArea({origin, size});
+    const isIn = isInScannableArea({ origin, size });
     setCodeInFrame(isIn);
 
     if (!isIn) return;
@@ -127,16 +127,7 @@ export default function CameraScanner({
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#3A5073" />
-      <View style={{ position: 'absolute', top: y, left: x, width: width, height: height, borderWidth: 2, borderColor: "red" }}>
-
-      </View>
-      {/* Header */}
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Scanner de Código</Text>
-        <Text style={styles.headerSubtitle}>Posicione o código dentro do quadro</Text>
-        <Text>X:{x} - Y:{y} - Width:{width} - Heigth:{height}</Text>
-      </View>
-
+      <View style={{ position: 'absolute', top: y, left: x, width: width, height: height, borderWidth: 2, borderColor: "red" }}></View>
       {/* Camera Container */}
       <View style={styles.cameraContainer}>
         <CameraView
@@ -147,34 +138,6 @@ export default function CameraScanner({
           }}
           onBarcodeScanned={handleBarcodeScanned}
         />
-        <View
-          style={styles.scanFrame}
-        >
-
-          {/* Indicadores de canto */}
-          <View style={[styles.corner, styles.topLeft, codeInFrame && styles.cornerActive]} />
-          <View style={[styles.corner, styles.topRight, codeInFrame && styles.cornerActive]} />
-          <View style={[styles.corner, styles.bottomLeft, codeInFrame && styles.cornerActive]} />
-          <View style={[styles.corner, styles.bottomRight, codeInFrame && styles.cornerActive]} />
-
-          {/* Linha de leitura animada */}
-          {isScanning && (
-            <Animated.View
-              style={[
-                styles.scanLine,
-                codeInFrame && styles.scanLineActive,
-                {
-                  transform: [{
-                    translateY: scanLineAnim.interpolate({
-                      inputRange: [0, 1],
-                      outputRange: [-90, windowHeight / 11],
-                    }),
-                  }],
-                },
-              ]}
-            />
-          )}
-        </View>
       </View>
 
       {/* Controles */}
@@ -201,8 +164,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: "100%",
     height: "100%",
-    flex: 1,
-    justifyContent: 'space-between',
     backgroundColor: '#3A5073',
   },
   modalOverlay: {
@@ -235,141 +196,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
   },
-  header: {
-    paddingTop: 60,
-    paddingHorizontal: 20,
-    paddingBottom: 30,
-    alignItems: 'center',
-  },
-  headerTitle: {
-    fontSize: 28,
-    fontWeight: '600',
-    color: 'white',
-    marginBottom: 8,
-  },
-  headerSubtitle: {
-    fontSize: 16,
-    color: 'rgba(255,255,255,0.8)',
-    textAlign: 'center',
-  },
-  statusContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 20,
-    marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  statusIcon: {
-    fontSize: 16,
-    marginRight: 8,
-  },
-  statusText: {
-    color: '#FF9500',
-    fontSize: 16,
-    fontWeight: '500',
-  },
   cameraContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     position: 'relative', // Importante para posicionamento absoluto do frame
   },
   camera: {
-    width: '90%',
-    height: 220,
-    borderRadius: 20,
+    position: 'absolute',
+    width: '100%',
+    height: "100%",
     overflow: 'hidden',
-  },
-  scanFrame: {
-    position: 'absolute',
-    width: '80%',
-    height: 180,
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 10,
-  },
-  corner: {
-    position: 'absolute',
-    width: 25,
-    height: 25,
-    borderColor: '#FFF',
-    borderWidth: 3,
-  },
-  cornerActive: {
-    borderColor: '#FFD700',
-    shadowColor: '#FFD700',
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 4,
-    elevation: 4,
-  },
-  topLeft: {
-    top: 0,
-    left: 0,
-    borderRightWidth: 0,
-    borderBottomWidth: 0,
-  },
-  topRight: {
-    top: 0,
-    right: 0,
-    borderLeftWidth: 0,
-    borderBottomWidth: 0,
-  },
-  bottomLeft: {
-    bottom: 0,
-    left: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-  },
-  bottomRight: {
-    bottom: 0,
-    right: 0,
-    borderLeftWidth: 0,
-    borderTopWidth: 0,
-  },
-  scanLine: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    height: 2,
-    backgroundColor: '#FFF',
-    shadowColor: '#FFF',
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 4,
-    elevation: 4,
-  },
-  scanLineActive: {
-    backgroundColor: '#FFD700',
-    shadowColor: '#FFD700',
-    height: 3,
-  },
-  instructionsContainer: {
-    backgroundColor: 'rgba(0,0,0,0.6)',
-    margin: 20,
-    padding: 16,
-    borderRadius: 12,
-  },
-  instructionsTitle: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '600',
-    marginBottom: 8,
-  },
-  instructionsText: {
-    color: 'rgba(255,255,255,0.8)',
-    fontSize: 14,
-    lineHeight: 20,
   },
   controlsContainer: {
     flexDirection: 'row',
